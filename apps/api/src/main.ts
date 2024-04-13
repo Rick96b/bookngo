87cddd5e16f62c3af2f6dcb39173a6a57db35751
Emil <yagufarove@gmail.com>
2024-04-13 16:54:56 +0500

    добавил в main.ts PrismaService, чтобы при запуске подключалась бд

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.get(PrismaService);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
