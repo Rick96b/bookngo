988dc198e044dae53041e5e8d982a09cbc9fa95c
Emil <yagufarove@gmail.com>
2024-04-13 16:57:09 +0500

    контроллер для auth

---
+++ apps/api/src/app/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post,Body, HttpCode} from '@nestjs/common';
+import { AuthService } from './auth.service';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { UserDto } from '../../../../../libs/models/user-dto.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private _authService: AuthService) {}
+  @Post('signUp')
+  @HttpCode(200)
+  async signUp(@Body() dto: UserDto) {
+    return this._authService.register(dto)
+  }
+  @Post('signIn')
+  @HttpCode(200)
+  async signIn(@Body() dto: UserDto) {
+    return this._authService.login(dto);
+  }
+
+}
