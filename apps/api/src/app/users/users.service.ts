e422a071c96a70796ebacb819484d1f3b8b4ac34
Emil <yagufarove@gmail.com>
2024-04-22 21:20:02 +0500

    user retrieval service

---
+++ apps/api/src/app/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma.service';
+import { User } from '@prisma/client';
+
+
+@Injectable()
+export class UsersService {
+
+  constructor(private _prismaService: PrismaService) {
+  }
+  async findOne(req: any): Promise<User> {
+    const user: User | null = await this._prismaService.user.findUnique({
+      where: {
+        email: req.user.email
+      }
+    });
+
+    return user;
+  }
+
+  async findAll() {
+    const users: User[] | null = await this._prismaService.user.findMany();
+    users.forEach((item: User) => delete item.password);
+    return users;
+  }
+}
