4f5bf4bca22e4dbf5cd9335fd23fb6acf72cc0f5
Emil <yagufarove@gmail.com>
2024-04-23 15:51:49 +0500

    подписался в компоненте на глобальное событие (auth). когда приходит новое событие, значение записывается в isLoggedIn, при destroy происходит отписка

--- apps/bookngo/src/app/app.component.ts
+++ apps/bookngo/src/app/app.component.ts
@@ -1,22 +1,35 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
-import { Component } from '@angular/core';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from '@taiga-ui/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AuthService } from "./modules/common/services/auth.service";
-import { TabBarComponent } from "./modules/tab-bar";
+import { AuthService } from './modules/common/services/auth.service';
+import { TabBarComponent } from './modules/tab-bar';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TabBarComponent],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+    standalone: true,
+    imports: [RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TabBarComponent],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
-export class AppComponent {
-  constructor(
-    private authService: AuthService
-  ){}
-  title = 'bookngo';
-  isLoggedIn = this.authService.isLoggedIn
+export class AppComponent implements OnInit, OnDestroy{
+    constructor(public authService: AuthService) {
+    }
+
+    title = 'bookngo';
+    private destroy$: Subject<void> = new Subject<void>();
+    protected isLoggedIn: boolean;
+
+    ngOnInit(): void {
+        this.authService.getAuthState().pipe(
+            tap((state: boolean) => this.isLoggedIn = state),
+            takeUntil(this.destroy$)
+        ).subscribe()
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+    }
 }
