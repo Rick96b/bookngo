ae4d17a67b29ba5819d661de1b02c4ce9794f33f
Emil <yagufarove@gmail.com>
2024-04-23 23:23:18 +0500

    подписка на календарь

--- apps/bookngo/src/app/modules/calendar/components/calendar/calendar.component.ts
+++ apps/bookngo/src/app/modules/calendar/components/calendar/calendar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { generateCalendar } from '../../utils/generateCalendar';
 import { CalendarService } from '../../sevices/calendar.service';
 import { DatePickerComponent } from '../date-picker/date-picker.component';
 import { Day } from '../../model/day.interface';
 import { TuiMonthPipe } from '../../pipes/TuiMonth.pipe';
+import { Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
     standalone: true,
@@ -15,23 +16,29 @@ import { TuiMonthPipe } from '../../pipes/TuiMonth.pipe';
     templateUrl: './calendar.component.html',
     styleUrl: './calendar.component.scss'
 })
-export class CalendarComponent implements OnInit {
-    days: Day[] = []
-    month: {year: number, month: number}
-    constructor(
-        private calendarService: CalendarService
-    ) {        
+export class CalendarComponent implements OnInit, OnDestroy {
+    protected days: Day[] = [];
+    protected month: { year: number, month: number };
 
+    private destroy$: Subject<void> = new Subject<void>();
+
+    constructor(private calendarService: CalendarService) {
+    }
+    ngOnInit(): void {
+        this.calendarService.getDate$().pipe(
+            tap((date) => {
+                this.days = generateCalendar(date.year, date.month);
+                this.month = date;
+            }),
+            takeUntil(this.destroy$)
+        ).subscribe();
     }
 
-    ngOnInit(): void { 
-        this.calendarService.getDate$().subscribe((date) => {
-            this.days = generateCalendar(date.year, date.month)
-            this.month = date
-        })
+    changeMonth({ year, month }: { year: number, month: number }): void {
+        this.calendarService.setDate(year, month);
     }
 
-    changeMonth({year, month}: {year: number, month: number}) {
-        this.calendarService.setDate(year, month)
+    ngOnDestroy(): void {
+        this.destroy$.next();
     }
 }
