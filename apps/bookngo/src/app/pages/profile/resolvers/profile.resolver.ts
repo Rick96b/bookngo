fb0106507093b0de400c0e47e6b6a4a57258c340
Emil <yagufarove@gmail.com>
2024-04-25 20:34:50 +0500

    типизировал резольвер

--- apps/bookngo/src/app/pages/profile/resolvers/profile.resolver.ts
+++ apps/bookngo/src/app/pages/profile/resolvers/profile.resolver.ts
@@ -1,10 +1,11 @@
 import { ResolveFn } from '@angular/router';
-import { ProfileService } from '../services/profile.service';
+import { UserService } from '../../../modules/common/services/user.service';
 import { inject } from '@angular/core';
 import { catchError, of } from 'rxjs';
+import { User } from '@bookngo/base';
 
-export const profileResolver: ResolveFn<any> = () => {
-    return inject(ProfileService).getUser().pipe(
+export const profileResolver: ResolveFn<User | null> = () => {
+    return inject(UserService).getUser().pipe(
         catchError(() => {
             console.log('Ошибка при загрузке данных пользователя');
             return of(null);
