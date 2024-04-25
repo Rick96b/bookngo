1aedd4ba4af56b74ee6a463657c44824d64506bc
Emil <yagufarove@gmail.com>
2024-04-25 22:45:24 +0500

    идет запрос на регистрацию -> возвращается токен, как только приходит ответ выполняется второй запрос на компанию

--- apps/bookngo/src/app/modules/register/data/services/register.service.ts
+++ apps/bookngo/src/app/modules/register/data/services/register.service.ts
@@ -1,37 +1,51 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { CompanyInDto, UserBaseInfoDto } from '@common'
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CompanyInDto, UserBaseInfoDto } from '@common';
 import { jwtDecode } from 'jwt-decode';
+import { catchError, Observable, of, switchMap, tap } from 'rxjs';
+import { AuthService } from '@bookngo/base';
+import { BASE_URL } from '../../../common/tokens/base-url.token';
 
 
 @Injectable({
-    providedIn: 'root',
+    providedIn: 'root'
 })
 export class RegisterService {
-  
+
     constructor(
+        @Inject(BASE_URL) private _baseUrl: string,
         private http: HttpClient,
-    ){ }
-      
-    registerUser(user: UserBaseInfoDto) {
-        let token = ''
-        this.http.post<{token: string}>("http://localhost:3000/api/auth/signUp", user).subscribe({
-            next: (res) => {
-                token = res.token
-                const data = jwtDecode<{email: string, id: number}>(res.token)
-                this.addCompany({
-                    name: user.companyName,
-                    ceo: data.id,
-                    departments: [],
-                    employees: []
+        private authService: AuthService
+    ) {
+    }
+
+    //временная типизация
+    registerUser(user: UserBaseInfoDto): Observable<null | object> {
+        return this.http.post<{ token: string }>(`${this._baseUrl}/auth/signUp`, user)
+            .pipe(
+                tap(({ token }): void => {
+                    this.authService.loginByToken(token);
+                }),
+                switchMap(({ token }) => {
+                    const data = jwtDecode<{ email: string, id: number }>(token);
+                    return this.addCompany({
+                        name: user.companyName,
+                        ceo: data.id,
+                        departments: [],
+                        employees: []
+                    });
                 })
-            },
-            error: (err) => console.log(err)
-        })
-        return token
+            );
+
     }
 
     addCompany(company: CompanyInDto) {
-        return this.http.post("http://localhost:3000/api/auth/signUp", company)
+        return this.http.post(`${this._baseUrl}/auth/signUp`, company)
+            .pipe(
+                catchError((err) => {
+                    console.log(err);
+                    return of(null);
+                })
+            );
     }
 }
