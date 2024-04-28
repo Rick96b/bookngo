f4d342b42209c2cb84fcf56a1df3f2fa6d3740b3
Emil Yagufarov <yagufarove@gmail.com>
2024-04-28 14:37:52 +0500

    чутка потипизировал, пока разбирался

--- apps/bookngo/src/app/base/services/company.service.ts
+++ apps/bookngo/src/app/base/services/company.service.ts
@@ -1,41 +1,48 @@
-import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, filter, map } from 'rxjs';
-import { BASE_URL } from '../../modules/common/tokens/base-url.token';
-import { Company, getRequestOptions, User } from '@bookngo/base';
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable, filter, map, tap, catchError, of} from 'rxjs';
+import {BASE_URL} from '../../modules/common/tokens/base-url.token';
+import {Company, getRequestOptions, User} from '@bookngo/base';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
-  private _users$ = new BehaviorSubject<User[]>([])
+
+  private _users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
   private _company$ = new BehaviorSubject<Company | null>(null)
-  constructor(private _httpClient: HttpClient, @Inject(BASE_URL) private _baseUrl: string) {
-    this.fetchUsers().subscribe({
-      next: (res) => this._users$.next(res)
-    })
+
+  constructor(@Inject(BASE_URL) private _baseUrl: string, private _httpClient: HttpClient) {
+    this.fetchUsers()
+      .pipe(
+        tap((users: User[]) => this._users$.next(users)),
+        catchError((err) => {
+          console.error(err);
+          return of(null);
+        })
+      ).subscribe();
   }
 
   public getMe(): Observable<User> {
-    return this.fetchMe()
+    return this.fetchMe();
   }
 
   public getUser(userId: number): Observable<User | undefined> {
     return this._users$.pipe(
-      map(users => users.find(user => user.id === userId))
-    )
+      map((users: User[]) => users.find((user: User)=> user.id === userId))
+    );
   }
 
   public getAllUsers(): Observable<User[]> {
     return this._users$.asObservable()
   }
 
-  fetchUsers() {
-    return this._httpClient.get<User[]>(`${this._baseUrl}/users/getAll`, getRequestOptions()) as Observable<User[]>;
+  private fetchUsers(): Observable<User[]> {
+    return this._httpClient.get<User[]>(`${this._baseUrl}/users/getAll`, getRequestOptions());
   }
 
-  fetchMe() {
-    return this._httpClient.get<User>(`${this._baseUrl}/users/getOne`, getRequestOptions()) 
+  private fetchMe(): Observable<User> {
+    return this._httpClient.get<User>(`${this._baseUrl}/users/getOne`, getRequestOptions());
   }
 }
 
