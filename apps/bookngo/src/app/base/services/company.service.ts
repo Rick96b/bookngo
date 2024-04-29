233c169125c9150ebe10aa4137147b8ac07c645e
Rick96b <olegkorobeinikov21@gmail.com>
2024-04-29 14:11:49 +0500

    Добавил поток юзера в сервис

--- apps/bookngo/src/app/base/services/company.service.ts
+++ apps/bookngo/src/app/base/services/company.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, filter, map } from 'rxjs';
+import { BehaviorSubject, Observable, filter, map, switchMap } from 'rxjs';
 import { BASE_URL } from '../../modules/common/tokens/base-url.token';
 import { Company, getRequestOptions, User } from '@bookngo/base';
 
@@ -8,16 +8,24 @@ import { Company, getRequestOptions, User } from '@bookngo/base';
   providedIn: 'root'
 })
 export class CompanyService {
+  private _me$ = new BehaviorSubject<User | null>(null)
   private _users$ = new BehaviorSubject<User[]>([])
   private _company$ = new BehaviorSubject<Company | null>(null)
+  
   constructor(private _httpClient: HttpClient, @Inject(BASE_URL) private _baseUrl: string) {
     this.fetchUsers().subscribe({
       next: (res) => this._users$.next(res)
     })
+    this.fetchMe().subscribe({
+      next: (res) => this._me$.next(res)
+    })
+    this.fetchCompany().subscribe({
+      next: (res) => this._company$.next(res)
+    })
   }
 
-  public getMe(): Observable<User> {
-    return this.fetchMe()
+  public getMe(): Observable<User | null> {
+    return this._me$.asObservable()
   }
 
   public getUser(userId: number): Observable<User | undefined> {
@@ -36,6 +44,12 @@ export class CompanyService {
 
   fetchMe() {
     return this._httpClient.get<User>(`${this._baseUrl}/users/getOne`, getRequestOptions()) 
+  }
+
+  fetchCompany() {
+    return this._httpClient.get<User>(`${this._baseUrl}/users/getOne`, getRequestOptions()).pipe(
+      switchMap(user => this._httpClient.get<Company>(`${this._baseUrl}/company/company/:${user.companyName}`))
+    )
   }
 }
 
