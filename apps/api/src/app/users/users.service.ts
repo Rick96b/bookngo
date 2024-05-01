9930c083878bc73b119bef9154a746d34e706207
Emil <yagufarove@gmail.com>
2024-05-01 23:19:37 +0500

    добавил эндпоинт для обновления данных пользователя

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -1,27 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
 import { User } from '@prisma/client';
+import { UserDto } from '@common';
 
 
 @Injectable()
 export class UsersService {
 
-  constructor(private _prismaService: PrismaService) {
-  }
-  async findOne(req: any): Promise<User> {
-    //протипзировать
-    const user: User | null = await this._prismaService.user.findUnique({
-      where: {
-        email: req.user.email
-      }
-    });
+    constructor(private _prismaService: PrismaService) {
+    }
 
-    return user;
-  }
+    async findOne(req: any): Promise<User> {
+        //протипзировать
+        const user: User | null = await this._prismaService.user.findUnique({
+            where: {
+                email: req.user.email
+            }
+        });
 
-  async findAll() {
-    const users: User[] | null = await this._prismaService.user.findMany();
-    users.forEach((item: User) => delete item.password);
-    return users;
-  }
+        return user;
+    }
+
+    async findAll() {
+        const users: User[] | null = await this._prismaService.user.findMany();
+        users.forEach((item: User) => delete item.password);
+        return users;
+    }
+
+    async updateOne(dto: UserDto) {
+        return this._prismaService.user.update({
+            where: {
+                email: dto.email
+            },
+            data: {
+                ...dto
+            }
+        });
+    }
 }
