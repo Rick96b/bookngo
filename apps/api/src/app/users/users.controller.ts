8519ec49dd8283d0f346e643c90a776511d2d3bf
Emil <yagufarove@gmail.com>
2024-05-01 23:19:33 +0500

    добавил эндпоинт для обновления данных пользователя

--- apps/api/src/app/users/users.controller.ts
+++ apps/api/src/app/users/users.controller.ts
@@ -1,23 +1,30 @@
-import { Controller, Get, UseGuards, Param, Req } from '@nestjs/common';
+import { Controller, Get, UseGuards, Param, Req, Body, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { UserDto } from '@common';
 
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {
-  }
+    constructor(private readonly usersService: UsersService) {
+    }
 
-  @Get('getOne')
-  @UseGuards(JwtAuthGuard)
-  getUser(@Req() req: Request) {
-    //
-    return this.usersService.findOne(req);
-  }
+    @Get('getOne')
+    @UseGuards(JwtAuthGuard)
+    getUser(@Req() req: Request) {
+        //
+        return this.usersService.findOne(req);
+    }
 
-  @Get('getAll')
-  @UseGuards(JwtAuthGuard)
-  getAllUsers(@Req() req: Request) {
-    return this.usersService.findAll();
-  }
+    @Get('getAll')
+    @UseGuards(JwtAuthGuard)
+    getAllUsers(@Req() req: Request) {
+        return this.usersService.findAll();
+    }
+
+    @Put('updateOne')
+    @UseGuards(JwtAuthGuard)
+    updateUser(@Body() dto: UserDto) {
+        return this.usersService.updateOne(dto);
+    }
 }
--- apps/bookngo/src/app/pages/profile/components/profile.component.ts
+++ apps/bookngo/src/app/pages/profile/components/profile.component.ts
@@ -2,17 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiAvatarModule, tuiAvatarOptionsProvider } from '@taiga-ui/kit';
 import { TabBarComponent } from '../../../modules/tab-bar';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { tap } from 'rxjs';
 import { BnButtonComponent } from '@bookngo/ui-components';
 import { PositionTransformPipe } from '../pipes/position-transform.pipe';
 import { TuiForModule } from '@taiga-ui/cdk';
 import { User } from '@bookngo/base';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
     selector: 'app-profile',
     standalone: true,
-    imports: [CommonModule, TabBarComponent, TuiAvatarModule, BnButtonComponent, PositionTransformPipe, TuiForModule],
+    imports: [CommonModule, TabBarComponent, TuiAvatarModule, BnButtonComponent, PositionTransformPipe, TuiForModule, RouterLink, TuiButtonModule],
     templateUrl: './profile.component.html',
     styleUrl: './profile.component.scss',
     providers: [
