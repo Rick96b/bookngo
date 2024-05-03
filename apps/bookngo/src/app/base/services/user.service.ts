45357e02a1e0603dac578af67dc22656d76dc1aa
Emil <yagufarove@gmail.com>
2024-05-03 09:45:42 +0500

    соль для обхода кеширования

--- apps/bookngo/src/app/base/services/user.service.ts
+++ apps/bookngo/src/app/base/services/user.service.ts
@@ -10,33 +10,37 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
 
     private _me$: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
-    public rnd: number;
+
     constructor(@Inject(BASE_URL) private _baseUrl: string, private _httpClient: HttpClient) {
-        this.rnd = Math.floor(Math.random() * 1000)
-        console.log('init service')
     }
-    public fetchMe(): Observable<User | null> {
-        console.log('1231231231');
-        return this._httpClient.get<User>(`${this._baseUrl}/users/getOne`, getRequestOptions()).pipe(
-            tap((user: User) => {
-                this._me$.next(user);
-            }),
-            catchError((err) => {
-                console.error(err);
-                return of(null);
-            })
-        )
-    }
+
     public getMe(): Observable<User | null> {
-        console.log(this._me$.getValue())
-        console.log( ' getMe')
         return this._me$.asObservable();
     }
 
-    public updateMe(data: User | null): void {
-         this._me$.next(data)
-        console.log(this._me$.getValue())
+    public fetchMe(): Observable<User | null> {
+        const salt: number = (new Date()).getTime();
+        return this._httpClient.get<User>(`${this._baseUrl}/users/getOne?${salt}`, getRequestOptions())
+            .pipe(
+                tap((user: User) => {
+                    this._me$.next(user);
+                }),
+                catchError((err) => {
+                    console.error(err);
+                    return of(null);
+                })
+            );
 
-        console.log('update')
+    }
+
+    public updateMe(body: User): Observable<User | null> {
+        return this._httpClient.put<User>(`${this._baseUrl}/users/updateOne`, body, getRequestOptions())
+            .pipe(
+                tap((user: User) => this._me$.next(user)),
+                catchError((err) => {
+                    console.error(err);
+                    return of(null);
+                })
+            );
     }
 }
