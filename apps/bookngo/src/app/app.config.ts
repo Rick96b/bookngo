39c020ddcea2bb09d346df2a05a05985dae85595
Emil <yagufarove@gmail.com>
2024-05-06 19:47:45 +0500

    добавил кастомный глобальный error handler

--- apps/bookngo/src/app/app.config.ts
+++ apps/bookngo/src/app/app.config.ts
@@ -1,12 +1,22 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TuiRootModule } from '@taiga-ui/core';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from '@bookngo/base';
+import { authInterceptor, CustomErrorHandler } from '@bookngo/base';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideClientHydration(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule), provideHttpClient(withInterceptors([authInterceptor]))],
+  providers: [
+      provideAnimations(),
+      provideClientHydration(),
+      provideRouter(appRoutes),
+      importProvidersFrom(TuiRootModule),
+      provideHttpClient(withInterceptors([authInterceptor])),
+      {
+          provide: ErrorHandler,
+          useClass: CustomErrorHandler
+      }
+  ]
 };
