8d8f26272c95e89726187c682f51525f10960d46
Emil <yagufarove@gmail.com>
2024-05-06 15:22:26 +0500

    добавил интерцептор в конфиг

--- apps/bookngo/src/app/app.config.ts
+++ apps/bookngo/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { TuiRootModule } from "@taiga-ui/core";
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { TuiRootModule } from '@taiga-ui/core';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from '@bookngo/base';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideClientHydration(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule), provideHttpClient()],
+  providers: [provideAnimations(), provideClientHydration(), provideRouter(appRoutes), importProvidersFrom(TuiRootModule), provideHttpClient(withInterceptors([authInterceptor]))],
 };
