44dd0a0af984a618d0491110dc20a35503a8756b
Emil <yagufarove@gmail.com>
2024-05-06 17:41:06 +0500

    типизация

--- apps/bookngo/src/app/base/interceptors/auth.interceptor.ts
+++ apps/bookngo/src/app/base/interceptors/auth.interceptor.ts
@@ -1,11 +1,12 @@
-import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '@bookngo/base';
+import { Observable } from 'rxjs';
 
-export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
     const authToken: string = inject(AuthService).getAuthToken();
     if (authToken) {
-        const newReq: HttpRequest<unknown> = req.clone({ headers: req.headers.append('Authorization', `Bearer ${authToken}`) });
+        const newReq: HttpRequest<unknown> = req.clone({ setHeaders: { Authorization: `Bearer ${authToken}` } });
         return next(newReq);
     }
     return next(req);
