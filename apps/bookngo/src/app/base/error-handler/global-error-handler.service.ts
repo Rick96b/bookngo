6b08ef66fd8c71b76a2d47cbcfdaa5f89226f556
Emil <yagufarove@gmail.com>
2024-05-06 21:41:42 +0500

    логгирование глобальной ошибки + роутинг на страницу ошибки

--- apps/bookngo/src/app/base/error-handler/global-error-handler.service.ts
+++ apps/bookngo/src/app/base/error-handler/global-error-handler.service.ts
@@ -1,14 +1,16 @@
 import { ErrorHandler, Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable()
-export class CustomErrorHandler implements ErrorHandler {
+export class GlobalErrorHandler implements ErrorHandler {
 
-    constructor(private zone: NgZone) {
+    constructor(private zone: NgZone, private _router: Router) {
     }
 
     handleError(error: unknown): void {
         this.zone.run((): void => {
             console.error('Caught by Custom Error Handler: ', error);
+            this._router.navigate(['error']);
         });
     }
 }
