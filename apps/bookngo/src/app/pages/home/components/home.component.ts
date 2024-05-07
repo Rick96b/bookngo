14bba8b6678fc60c6f1bc7440c044833c42e3cce
Emil <yagufarove@gmail.com>
2024-05-07 20:19:31 +0500

    ненужный инстанс

--- apps/bookngo/src/app/pages/home/components/home.component.ts
+++ apps/bookngo/src/app/pages/home/components/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../../../modules/calendar';
 import { UsersListComponent } from '../../../modules/users-list';
-import { CompanyService } from '@bookngo/base';
 import { DepartmentService } from '../services/department.service';
 
 @Component({
@@ -16,7 +15,7 @@ import { DepartmentService } from '../services/department.service';
     providers: [DepartmentService]
 })
 export class HomePageComponent implements OnInit {
-    constructor(public companyService: CompanyService) {
+    constructor() {
     }
 
     ngOnInit(): void {
