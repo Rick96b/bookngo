41838191af9255d9dcb19de8a37ee54d916a5ff3
Emil <yagufarove@gmail.com>
2024-05-09 19:07:38 +0500

    deleted unused methods

--- apps/bookngo/src/app/pages/profile/components/profile.component.ts
+++ apps/bookngo/src/app/pages/profile/components/profile.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiAvatarModule, tuiAvatarOptionsProvider } from '@taiga-ui/kit';
 import { TabBarComponent } from '../../../modules/tab-bar';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { PositionTransformPipe } from '../pipes/position-transform.pipe';
 import { TuiForModule } from '@taiga-ui/cdk';
 import { DestroyService, UserService } from '@bookngo/base';
@@ -12,30 +12,22 @@ import { FormatMonthDatePipe } from '../pipes/format-month-date.pipe';
 import { FormatDayDatePipe } from '../pipes/format-day-date.pipe';
 
 @Component({
-  selector: 'app-profile',
-  standalone: true,
+    selector: 'app-profile',
+    standalone: true,
     imports: [CommonModule, TabBarComponent, TuiAvatarModule, PositionTransformPipe, TuiForModule, RouterLink, TuiButtonModule, TuiFormatPhonePipeModule, FormatVacationDateDirective, FormatMonthDatePipe, FormatDayDatePipe],
-  templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss',
-  providers: [
-    tuiAvatarOptionsProvider({
-      size: 'xl',
-      autoColor: true,
-      rounded: true
-    }),
-      DestroyService
-  ]
+    templateUrl: './profile.component.html',
+    styleUrl: './profile.component.scss',
+    providers: [
+        tuiAvatarOptionsProvider({
+            size: 'xl',
+            autoColor: true,
+            rounded: true
+        }),
+        DestroyService
+    ]
 })
 export class ProfileComponent {
 
-  constructor(private _router: Router, protected _userService: UserService) {
-  }
-
-  protected reloadPage(): void {
-    window.location.reload();
-  }
-
-  protected navigateToHome(): void {
-    this._router.navigate(['cabinet']);
-  }
+    constructor(protected _userService: UserService) {
+    }
 }
