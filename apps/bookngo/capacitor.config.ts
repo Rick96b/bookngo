f8019318db4f9d7a8d1f67de64f9c5480eee0810
Rick96b <olegkorobeinikov21@gmail.com>
2024-05-19 17:56:22 +0500

    Жесть коммит

---
+++ apps/bookngo/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bookngo.angular',
+  appName: 'Bookngo',
+  webDir: '../../dist/apps/bookngo/browser',
+};
+
+export default config;
+
