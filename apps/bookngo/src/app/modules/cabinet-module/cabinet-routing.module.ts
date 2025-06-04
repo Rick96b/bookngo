ce5d84b66d3a7ee2abe76df7e7903cfdeb026fad
eiagufarov <eiagufarov@abanking.ru>
2025-06-04 21:19:29 +0500

    retr

--- apps/bookngo/src/app/modules/cabinet-module/cabinet-routing.module.ts
+++ apps/bookngo/src/app/modules/cabinet-module/cabinet-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { cabinetRoutes } from './cabinet-routes';
-import { CabinetComponent } from './components/cabinet.component';
-import { TabBarComponent } from '../tab-bar';
 import { CompanyService, DestroyService, UserService } from '@bookngo/base';
 import { NotificationsService } from '../notifications/services/notifications.service';
 
 @NgModule({
-    imports: [CommonModule, RouterModule.forChild(cabinetRoutes), TabBarComponent],
+    imports: [CommonModule, RouterModule.forChild(cabinetRoutes)],
     exports: [RouterModule],
-    declarations: [CabinetComponent],
-    bootstrap: [CabinetComponent],
     providers: [UserService, CompanyService, NotificationsService, DestroyService]
 })
 export class CabinetRoutingModule {}
--- apps/bookngo/src/app/modules/cabinet-module/components/cabinet.component.ts
+++ apps/bookngo/src/app/modules/cabinet-module/components/cabinet.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { TabBarComponent } from '../../tab-bar/components/tab-bar.component';
 
 @Component({
     selector: 'app-cabinet',
     templateUrl: './cabinet.component.html',
+    standalone: true,
+    imports: [RouterModule, TabBarComponent],
+    animations: [
+        trigger('routeAnimations', [
+            transition('* <=> *', [
+                style({ opacity: 0, transform: 'translateY(10px)' }),
+                animate('300ms ease-out',
+                    style({ opacity: 1, transform: 'translateY(0)' })
+                )
+            ])
+        ])
+    ]
 })
 export class CabinetComponent {
+    prepareRoute(outlet: any) {
+        return outlet?.activatedRouteData?.['animation'] || outlet;
+    }
 }
